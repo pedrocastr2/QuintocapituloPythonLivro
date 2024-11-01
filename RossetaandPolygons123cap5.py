1be12d4100e0b6387cf020c37d82a01698e8c8c0
pedrocastr <102086416+pedrocastr2@users.noreply.github.com>
2024-11-01 00:51:29 -0300

    Add files via upload

---
+++ RossetaandPolygons123cap5.py
@@ -0,0 +1,23 @@
+import turtle
+t = turtle.Pen()
+#pede o número de lados ou de circulos ao usuário ,com default igual a 4
+sides = int(turtle.numinput("Qual número de circulos ", "Quantos lados  em sua forma? ",4))
+
+#Laço para a espiral externa de polígonos e rosetas com tamanho de 5 a 75
+
+for m in range(5,75):
+     t.left(360/sides+5) 
+     t.width(m//25+1) # Modifica a caneta e // = divisão inteira
+     t.penup() #Não desenha linhas na espiral
+     t.forward(m*4) #Move para o próximo canto
+     t.pendown() #
+     # Desenha uma pequena roseta em cada canto PAR da espiral
+     if ( m % 2 == 0):
+         for n in range(sides):
+             t.circle(m / 3)
+             t.right(360 / sides)
+        # Desenha um pequeno círculo em cada canto ÍMPAR da espiral
+     else:
+         for n in range(sides):
+             t.forward(m)
+             t.right(360 / sides)
