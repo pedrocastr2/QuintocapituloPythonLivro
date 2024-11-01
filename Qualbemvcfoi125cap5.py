8c4f5012c1ef062e20ebe88e903331cd45d46cc7
pedrocastr <102086416+pedrocastr2@users.noreply.github.com>
2024-11-01 17:03:12 -0300

    Add files via upload

---
+++ Qualbemvcfoi125cap5.py
@@ -0,0 +1,12 @@
+nota = eval(input("Entre com sua nota(0-100): "))
+print("--------------------------------------------------------------------")
+if nota>= 90:
+    print("Meus parbéns , você é um aluno nota 10, Você foi muito bem ")
+elif nota>= 80:
+     print("Meus parbéns ,você é um aluno nota 8, Você foi bem")
+elif nota>= 70:
+     print("Você foi bem,você é um aluno nota 7 ,mas pode melhorar")
+elif nota>= 60:
+     print("Você está na média e pode melhorar")
+else:
+    print("Infelizmente você não tirou o necessário para passar")
